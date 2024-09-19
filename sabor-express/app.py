a891fd9c7f28dc94eaa102a18fa574d72c4cc6a5
Karen Cristina <karen-kaah@hotmail.com>
2024-09-19 17:58:21 -0300

    python

---
+++ sabor-express/app.py
@@ -0,0 +1,35 @@
+import os
+
+
+
+
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+    os.system('cls')
+    print('encerrando o app\n')
+
+if opcao_escolhida==1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida==2:
+    print('Listar Restaurantes') 
+elif opcao_escolhida==3:
+    print("Ativar Restaurante")
+
+else:
+    finalizar_app()
+    
