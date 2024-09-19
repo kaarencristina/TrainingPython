284535eb426ca89cc739ca356efbb8a14971effe
Karen Cristina <karen-kaah@hotmail.com>
2024-09-19 18:06:45 -0300

    funcoes

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,35 +1,41 @@
 import os
 
 
+def exibir_nome_do_programa():
+    print("Sabor-express")
 
 
-print("""
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
-""")
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
 
 def finalizar_app():
     os.system('cls')
     print('encerrando o app\n')
 
-if opcao_escolhida==1:
-    print('Cadastrar Restaurante')
-elif opcao_escolhida==2:
-    print('Listar Restaurantes') 
-elif opcao_escolhida==3:
-    print("Ativar Restaurante")
 
-else:
-    finalizar_app()
+def escolher_opcao():
+
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida==1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida==2:
+        print('Listar Restaurantes') 
+    elif opcao_escolhida==3:
+        print("Ativar Restaurante")
+
+    else:
+        finalizar_app()
     
+def main():
+    exibir_nome_do_programa();
+
+if __name__=='__main__':
+    main()
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
